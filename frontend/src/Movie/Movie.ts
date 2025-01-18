Radarr/Radarr

New Custom Filters based on Availability
https://github.com/Radarr/Radarr/issues/2074

make the UI take into account AvailabilityDelay
to make it consistent with RSS Decision Engine

add custom filter options for minimumAvailabilityDate
with and without Delay

Simplify the filter names.
ConsideredAvailable ---> DateConsideredAvailable  (these always take into account the delay)
MinimumAvailability ---> MinimumAvailabilityDate  (these never take into account delay)

if delay = 0 .. the 2 dates above are identical.
if delay = 0, MimimumAvailabilityDate will be 1 of InCinemas, Announced or Physical/Digital (Released) dates

Add ability to make the custom filter dates relative to
currentdate

DateConsideredAvailable tooltip on NotAvailable

frontend/src/Movie/Movie.ts
@@ -71,6 +71,7 @@ interface Movie extends ModelBase {
   movieFile: MovieFile;
   hasFile: boolean;
   isAvailable: boolean;
+  isAvailableDate: string;
   isSaving?: boolean;
 }
 

frontend/src/Store/Actions/movieActions.js
@@ -152,6 +152,14 @@ export const filterPredicates = {
     return dateFilterPredicate(item.physicalRelease, filterValue, type);
   },
 
+  isAvailableDate: function(item, filterValue, type) {
+    return dateFilterPredicate(item.isAvailableDate, filterValue, type);
+  },
+
+  minimumAvailabilityDate: function(item, filterValue, type) {
+    return dateFilterPredicate(item.minimumAvailabilityDate, filterValue, type);
+  },
+
   digitalRelease: function(item, filterValue, type) {
     return dateFilterPredicate(item.digitalRelease, filterValue, type);
   },

geogolem/RadarrPublic

frontend/src/Movie/Movie.ts
@@ -81,6 +81,7 @@ interface Movie extends ModelBase {
   hasFile: boolean;
   lastSearchTime?: string;
   isAvailable: boolean;
+  dateConsideredAvailable: string;
   isSaving?: boolean;
 }
 

frontend/src/Store/Actions/movieActions.js
@@ -152,6 +152,14 @@ export const filterPredicates = {
     return dateFilterPredicate(item.physicalRelease, filterValue, type);
   },
 
+  dateConsideredAvailable: function(item, filterValue, type) {
+    return dateFilterPredicate(item.dateConsideredAvailable, filterValue, type);
+  },
+
+  minimumAvailabilityDate: function(item, filterValue, type) {
+    return dateFilterPredicate(item.minimumAvailabilityDate, filterValue, type);
+  },
+
   digitalRelease: function(item, filterValue, type) {
     return dateFilterPredicate(item.digitalRelease, filterValue, type);
   },